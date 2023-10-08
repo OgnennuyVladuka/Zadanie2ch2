77d442891708e9f69b3c3fcf09c02d2ff734f6d8
OgnennuyVladuka <146809006+OgnennuyVladuka@users.noreply.github.com>
2023-10-08 22:32:16 +0300

    Add files via upload

---
+++ z2l/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace z2l
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sred = 0;
+            double[] cena = new double[20];
+            for (int i = 0; i < 20; i++)
+            {
+                cena[i] = Convert.ToInt32(Console.ReadLine());
+                sred = sred + cena[i];
+            }
+            sred = sred / 20;
+            Console.WriteLine("Средняя стоимость товаров: " + sred);
+            int kol = 0;
+            for (int i = 0;i < 20; i++)
+            {
+                if (cena[i] < sred)
+                {
+                    kol++;
+                }
+            }
+            Console.WriteLine("Количество товаров со стоимостью меньше, чем средняя: " + kol);
+        }
+    }
+}
